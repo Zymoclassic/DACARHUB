ec7071d9d80f18d88d9da6db8e8d932888957862
Awopeju Kolawole Moses <Awopejumoses@gmail.com>
2024-02-03 10:51:15 +0100

    create user

--- dacarhub/project/views.py
+++ dacarhub/project/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.template import loader
 from .models import car
+
+# Create your views here.
+
 
 # Create your views here.
 def home(request):
@@ -44,6 +46,21 @@ def carview(request):
 
 
 def register(request):
-    template = loader.get_template('register.html')
-    return HttpResponse(template.render())
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+        if password == confirm_password:
+            if User.objects.filter(username=username).exists():
+                messages.info(request, 'Used email, please try another email.')
+                return redirect ('register')
+            else:
+                user = User.objects.create_user(username=username, password=password)
+                user.save()
+                return redirect ('login')
+        else:
+            messages.info(request, "Password doesn't match!" )
+            return redirect ('register')
+    else:
+        return render(request, 'register.html')
 
